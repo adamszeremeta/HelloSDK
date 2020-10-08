4d5d2662013c6e07a59bd9d95a27ad6bf26b816f
Szeremeta Adam <Adam.Szeremeta@dreamlab.pl>
2020-10-08 08:23:49 +0200

    Initial commit

---
+++ Example/HelloSDK/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HelloSDK
+//
+//  Created by Szeremeta Adam on 10/08/2020.
+//  Copyright (c) 2020 Szeremeta Adam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
